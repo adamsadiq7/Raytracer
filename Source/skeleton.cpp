dd097d994faadc3c4f9d3610478481b27b528683
Ashwinder Khurana <ak16625@my.bristol.ac.uk>
2019-02-19 15:55:38 +0000

    cartesian rotation implemented, need to make homogenous one

--- Source/skeleton.cpp
+++ Source/skeleton.cpp
@@ -65,20 +65,25 @@ void Draw(screen *screen)
 
   Intersection intersection;
 
-  float focalLength = (float) SCREEN_WIDTH;
-  
+  float focalLength = (float)SCREEN_WIDTH;
 
   vec3 colour(1.0, 0.0, 0.0);
-  for (int x = 0; x < SCREEN_WIDTH; x++){
-    for (int y = 0; y < SCREEN_HEIGHT; y++){
+  for (int x = 0; x < SCREEN_WIDTH; x++)
+  {
+    for (int y = 0; y < SCREEN_HEIGHT; y++)
+    {
       vec4 d(x - SCREEN_WIDTH / 2, y - SCREEN_WIDTH / 2, focalLength, 1);
       d = glm::normalize(d);
-      if (closestIntersection(cameraPos, d, triangles, intersection)){
+      if (closestIntersection(cameraPos, d, triangles, intersection))
+      {
         PutPixelSDL(screen, x, y, triangles[intersection.triangleIndex].color);
       }
     }
   }
 }
+
+
+
 
 // / Place updates of parameters here /
 bool Update()
@@ -88,11 +93,39 @@ bool Update()
   int t2 = SDL_GetTicks();
   float dt = float(t2 - t);
   t = t2;
-  vec4 moveForward(0,0,1,1);
-  vec4 moveBackward(0,0,-1,1);
-  vec4 moveLeft(-1,0,0,1);
-  vec4 moveRight(1,0,-1,1);
+
+  //---- Translation vectrors-----
+  vec4 moveForward(0, 0, 1, 1);
+  vec4 moveBackward(0, 0, -1, 1);
+  vec4 moveLeft(-1, 0, 0, 1);
+  vec4 moveRight(1, 0, -1, 1);
+  //------------------------------
+
+  //---- Rotation Matrix ---------
+  float theta =10 * 3.1415/180;
+
+  //------Left Rotation ----------
+  vec4 d1(cos(-theta), 0, sin(-theta), 1);
+  vec4 d2(0, 1, 0, 1);
+  vec4 d3(-sin(-theta), 0, cos(-theta), 1);
+  vec4 d4(0,0,0,1);
+  mat4 leftRot(d1, d2, d3,d4);
+  //------------------------------
+
+  //------Right Roation ----------
+  vec4 d1r(cos(theta), 0, sin(theta), 0);
+  vec4 d2r(0, 1, 0, 0);
+  vec4 d3r(-sin(theta), 0, cos(theta), 0);
+  vec4 d4r(0,0,0,1);
+  mat4 rightRot(d1r, d2r, d3r,d4r);
+  //------------------------------
+
   
+  // vec4 right(R[0][0], R[0][1],R[0][2], 1);
+  // vec4 down(R[1][0], R[1][1], R[1][2], 1);
+  // vec4 forward(R[2][0], R[2][1], R[2][2], 1);
+
+  //------------------------------
 
   SDL_Event e;
   while (SDL_PollEvent(&e))
@@ -106,36 +139,60 @@ bool Update()
       int key_code = e.key.keysym.sym;
       switch (key_code)
       {
-      case SDLK_UP:
+      case SDLK_UP:{
         /* Move camera forward */
-        
-        cameraPos = cameraPos + moveForward;
-        return true;
 
-      case SDLK_DOWN:
-        cameraPos = cameraPos + moveBackward;
-        return true;
-      case SDLK_LEFT:
-       cameraPos = cameraPos + moveLeft;
-        return true;
-      case SDLK_RIGHT:
-        cameraPos = cameraPos + moveRight;
-        return true;
-      case SDLK_ESCAPE:
-        /* Move camera quit */
-        return false;
+        cameraPos = cameraPos + moveForward;
+        // return true;
+        break;
       }
+      case SDLK_DOWN:{
+        cameraPos = cameraPos + moveBackward;
+        // return true;
+        break;
+      }
+      case SDLK_LEFT: {
+        std::cout << "x: " << cameraPos.x << "\n";
+        std::cout << "y: " << cameraPos.y << "\n";
+        std::cout << "z: " << cameraPos.z << "\n";
+      
+        cameraPos = cameraPos + leftRot;
+        
+
+        std::cout << "x': " << cameraPos.x << "\n";
+        std::cout << "y': " << cameraPos.y << "\n";
+        std::cout << "z': " << cameraPos.z << "\n";
+        // return true;
+        break;
+      }
+      case SDLK_RIGHT:{
+        cameraPos = rightRot * cameraPos;
+
+        // return true;
+        break;
+      }
+      case SDLK_ESCAPE:{
+        /* Move camera quit */
+        // return false;
+        break;
+      }
+      // case default:
+      //   break;
+      }
+
     }
   }
   return true;
 }
 
-bool closestIntersection(vec4 start, vec4 dir, vector<Triangle> &triangles, Intersection &closestIntersection){
+bool closestIntersection(vec4 start, vec4 dir, vector<Triangle> &triangles, Intersection &closestIntersection)
+{
 
   bool found = false;
 
   float lowest = std::numeric_limits<float>::max();
-  for (int i = 0; i < triangles.size(); ++i){
+  for (int i = 0; i < triangles.size(); ++i)
+  {
 
     vec4 v0 = triangles[i].v0;
     vec4 v1 = triangles[i].v1;
@@ -149,21 +206,24 @@ bool closestIntersection(vec4 start, vec4 dir, vector<Triangle> &triangles, Inte
 
     mat3 A(-d, e1, e2);
 
-   
     vec3 x = glm::inverse(A) * b;
 
     float t = x.x;
-    float u = x.y;
-    float v = x.z;
-    
+    if (t >= 0)
+    {
+      float u = x.y;
+      float v = x.z;
 
-    if ((u >= 0) && (v >= 0) && ((u + v) <= 1) && t>=0){
-      if (t < lowest){
-        closestIntersection.distance = t;
-        closestIntersection.position = start + t * dir;
-        closestIntersection.triangleIndex = i;
-        lowest = t;
-        found = true;
+      if ((u >= 0) && (v >= 0) && ((u + v) <= 1) && t >= 0)
+      {
+        if (t < lowest)
+        {
+          closestIntersection.distance = t;
+          closestIntersection.position = start + t * dir;
+          closestIntersection.triangleIndex = i;
+          lowest = t;
+          found = true;
+        }
       }
     }
   }
